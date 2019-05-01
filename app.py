ac880bf76fd7bfab9a3cc434873a10aaf93635d1
Masahiro Kukita <cookie@sfc.wide.ad.jp>
2019-05-02 04:15:59 +0900

    fix typo

--- app.py
+++ app.py
@@ -45,7 +45,7 @@ def callback():
 def handle_message(event):
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text=parser.answer(event.message.text))
+        TextSendMessage(text=parser.answer(event.message.text)))
 
 
 if __name__ == "__main__":
